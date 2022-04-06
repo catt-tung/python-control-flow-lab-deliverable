a732f52e39adb4a43e7026391a4a6f297a6af2bc
Catherine Tung <catt.tung@gmail.com>
2022-04-05 21:31:26 -0400

    solution 6

--- exercise-6.py
+++ exercise-6.py
@@ -19,3 +19,42 @@
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+
+input_month = input("Enter the month of the year (Jan - Dec): ")
+input_day = int(input("Enter the day of the month: "))
+
+winter = ["Dec", "Jan", "Feb", "Mar"]
+spring = ["Apr", "May"]
+summer = ["Jun", "Jul", "Aug", "Sep"]
+fall = ["Oct", "Nov"]
+
+if input_month in winter:
+  if input_month == "Jan" or input_month == "Feb":
+    print(f"{input_month} {input_day} is in winter.")
+  elif input_month == "Dec":
+    if input_day >= 21:
+      print(f"{input_month} {input_day} is in winter.")
+    else:
+      print(f"{input_month} {input_day} is in fall.")
+  elif input_month == "Mar":
+    if input_day <= 19:
+      print(f"{input_month} {input_day} is in winter.")
+    else:
+      print(f"{input_month} {input_day} is in spring.")
+elif input_month in spring:
+  print(f"{input_month} {input_day} is in spring.")
+elif input_month in summer:
+  if input_month == "Jul" or input_month == "Aug":
+    print(f"{input_month} {input_day} is in summer.")
+  elif input_month == "Jun":
+    if input_day >= 21:
+      print(f"{input_month} {input_day} is in summer.")
+    else:
+      print(f"{input_month} {input_day} is in spring.")
+  elif input_month == "Sep":
+    if input_day <= 21:
+      print(f"{input_month} {input_day} is in summer.")
+    else:
+      print(f"{input_month} {input_day} is in fall.")
+else:
+  print(f"{input_month} {input_day} is in fall.")
